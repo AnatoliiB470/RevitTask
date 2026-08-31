a9c4b407d6d434b7d835e4b0e833d67c69d67461
AnatoliiB470 <bortnuk470@gmail.com>
2026-08-31 14:09:38 +0300

    Implement message in the Revit 2025_2026 pop up plugin

--- Pop_Up.R25_26/Messanger.cs
+++ Pop_Up.R25_26/Messanger.cs
@@ -1,7 +1,16 @@
-﻿namespace Pop_Up.R25_26
-{
-    public class Messanger
-    {
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 
+namespace Pop_Up.R25_26
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Messanger : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Start Message", "Welcome to Revit");
+            return Result.Succeeded;
+        }
     }
 }
